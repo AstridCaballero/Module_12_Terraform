88c8661710aa6b6c8116ca70e72f20af05128d0b
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-06-15 16:56:53 +0100

    [Module_12] Add README.md file and original notes exported to pdf

---
+++ README.md
@@ -0,0 +1,13 @@
+![img.png](img.png)
+
+## Module 12 - Infrastructure as a Code with Terraform
+
+### Chapter 3 - Providers in Terraform
+
+Notes from chapter 3 were taken in notes app in freestyle, and it includes a mix of screenshots of TWN bootcamp as well as
+screenshots of my own work on the terminal and browser. I used AI and I have added that research to my notes.
+
+Notes have been exported into pdf format to be able to get the screenshots (avoiding retaking screenshots which is time-consuming)
+
+You can find original notes for
+- Chapter 3 [here](Module%2012%20-%20chapter%203_notes.pdf).
--- main.tf
+++ main.tf
@@ -1,25 +1,5 @@
 provider "aws" {
   region = "eu-west-2"
-  access_key = "AKIA2JGYSYB2DWZEUS47"
-  secret_key = "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"
+  access_key = "AKIA2JG4567890"
+  secret_key = "NicXYQornNGpNdl34567890"
 }
-
-#resource "aws_vpc" "development-vpc" {
-#  cidr_block = "10.0.0.0/16"
-#}
-#
-#resource "aws_subnet" "dev-subnet-1" {
-#  vpc_id = aws_vpc.development-vpc.id
-#  cidr_block = "10.0.10.0/24"
-#  availability_zone = "eu-west-2a"
-#}
-#
-#data "aws_vpc" "existing_vpc" {
-#  default = true
-#}
-#
-#resource "aws_subnet" "dev-subnet-2" {
-#  vpc_id = data.aws_vpc.existing_vpc.id
-#  cidr_block = "172.31.48.0/20"
-#  availability_zone = "eu-west-2a"
-#}
