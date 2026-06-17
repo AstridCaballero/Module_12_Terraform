815a1a2a05d2c8dd7a4da8d0585a60f9fc1cf7f4
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-06-17 12:41:42 +0100

    env vars in TF and README

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-  region = "eu-west-2"
-}
+provider "aws" {}
 
 variable "cidr_blocks" {
   description = "cidr blocks and name tags for vpc and subnets"
   type = list(object({
-    vpc_cidr = string
+    cidr_block = string
     name = string
   }))
 }
+
+variable "avail_zone" {}
 
 resource "aws_vpc" "development-vpc" {
   cidr_block = var.cidr_blocks[0].cidr_block
@@ -20,7 +20,7 @@ resource "aws_vpc" "development-vpc" {
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.development-vpc.id
   cidr_block = var.cidr_blocks[1].cidr_block
-  availability_zone = "eu-west-2a"
+  availability_zone = var.avail_zone
   tags = {
     Name: var.cidr_blocks[1].name
   }
