b71aa3b16bb4b2322d07697df0809467023473a9
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-06-22 15:52:09 +0100

    Add webserver module

--- main.tf
+++ main.tf
@@ -18,78 +18,14 @@ module "myapp-subnet" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
-resource "aws_default_security_group" "default-sg" {
-#  name = "myapp-sg"
+module "myapp-server" {
+  source = "./modules/webserver"
   vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "TCP"
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-
-  tags = {
-    Name: "${var.env_prefix}-default-sg"
-  }
-}
-
-#data "aws_ami" "latest-amazon-linux-image" {
-#  most_recent = true
-#  owners = ["amazon"]
-#  filter {
-#    name = "name"
-##    values = ["al2023-ami-*-kernel-*-x86_64"] // it returns a neuron image, so it needs more filtering
-#    values = ["al2023-ami-2023*-kernel-*-x86_64"] // it returns the image we want
-#  }
-#  filter {
-#    name = "virtualization-type"
-#    values = ["hvm"]
-#  }
-#}
-
-// use SSM parameter store to get the latest Amazon Linux 2023 image ID
-data "aws_ssm_parameter" "latest-amazon-linux-image" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-}
-
-// let EC2 instance know the public key to use for when user logs in via SSH using the private key
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami = data.aws_ssm_parameter.latest-amazon-linux-image.value
+  my_ip = var.my_ip
+  env_prefix = var.env_prefix
+  image_name = var.image_name
+  public_key_location = var.public_key_location
   instance_type = var.instance_type
-
   subnet_id = module.myapp-subnet.subnet.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh") // commented out to use provisioner instead
-
-  user_data_replace_on_change = true
-
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
+  avail_zone = var.avail_zone
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
+  value = module.myapp-server.instance_public_ip
 }
-
-#output "latest-amazon-linux-image-id" {
-#  value = data.aws_ami.latest-amazon-linux-image.id
-#}
