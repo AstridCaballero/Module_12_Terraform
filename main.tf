4e046e044a5bfa7a002b457e5802ff6a75db4033
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-06-22 12:50:20 +0100

    Add subnet module and README and notes

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable "public_key_location" {}
-variable "private_key_location" {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -18,49 +9,13 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name: "${var.env_prefix}-subnet-1"
-  }
-}
-
-#resource "aws_route_table" "myapp-route-table" {
-#  vpc_id = aws_vpc.myapp-vpc.id
-#
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.myapp-igw.id
-#  }
-#  tags = {
-#    Name: "${var.env_prefix}-rtb"
-#  }
-#}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name: "${var.env_prefix}-igw"
-  }
-}
-
-#resource "aws_route_table_association" "a-rtb-subnet" {
-#  subnet_id = aws_subnet.myapp-subnet-1.id
-#  route_table_id = aws_route_table.myapp-route-table.id
-#}
-
-resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name: "${var.env_prefix}-main-rtb"
-  }
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -107,14 +62,6 @@ resource "aws_default_security_group" "default-sg" {
 #    values = ["hvm"]
 #  }
 #}
-#
-#output "latest-amazon-linux-image-id" {
-#  value = data.aws_ami.latest-amazon-linux-image.id
-#}
-
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
 
 // use SSM parameter store to get the latest Amazon Linux 2023 image ID
 data "aws_ssm_parameter" "latest-amazon-linux-image" {
@@ -131,36 +78,16 @@ resource "aws_instance" "myapp-server" {
   ami = data.aws_ssm_parameter.latest-amazon-linux-image.value
   instance_type = var.instance_type
 
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  subnet_id = module.myapp-subnet.subnet.id
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
-#  user_data = file("entry-script.sh") // commented out to use provisioner instead
+  user_data = file("entry-script.sh") // commented out to use provisioner instead
 
   user_data_replace_on_change = true
-
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ec2-user"
-    private_key = file(var.private_key_location)
-  }
-
-#  provisioner "file" {
-#    source = "entry-script.sh"
-#    destination = "/home/ec2-user/entry-script-on-ec2.sh"
-#  }
-
-  provisioner "remote-exec" {
-    script = ["entry-script.sh"]
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ec2-public-ip.txt"
-  }
 
   tags = {
     Name = "${var.env_prefix}-server"
