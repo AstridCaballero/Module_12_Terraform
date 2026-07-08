181dbfbb9f92aa6672c28dd6b5441a9d10391a21
Astrid Caballero-Pardo <astridcaballero@gmail.com>
2026-07-08 15:22:40 +0100

    Monitor EKS cluster and add README

---
+++ eks-cluster.tf
@@ -0,0 +1,38 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.23.0"
+
+  name = "myapp-eks-cluster"
+  kubernetes_version = "1.36"
+  endpoint_public_access  = true
+  enable_cluster_creator_admin_permissions = true
+
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id = module.myapp-vpc.vpc_id
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t2.small"]
+    }
+  }
+}
